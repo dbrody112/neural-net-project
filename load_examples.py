5d997f66e81b9c7a13e3728652c07309fe285d7d
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2020-12-25 15:18:46 -0500

    Create load_examples.py

---
+++ load_examples.py
@@ -0,0 +1,29 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import math
+import os
+
+def load_dataset(filename):
+    examples = []
+    if(os.path.isfile(filename)):
+        with open(filename,'r') as file:
+            first_line = file.readline().split()
+            numExamples = int(first_line[0])
+            numDependentVars = int(first_line[1])
+            numTargets = int(first_line[2])
+            
+            for i in range(numExamples):
+                instance = file.readline().split()
+                instance = [float(i) for i in instance]
+                examples.append(instance)
+            examples = np.array(examples).T
+            x = examples[:numDependentVars].T
+    
+            assert x.shape[0]*x.shape[1] == numDependentVars*numExamples
+    
+            y = examples[numDependentVars:].T.astype('int')
+            
+            assert y.shape[0]*y.shape[1] == numTargets*numExamples
+        
+            return x,y
